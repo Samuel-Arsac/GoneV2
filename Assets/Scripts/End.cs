4ffe40b6c525236b6d8d6fdb7f7d89bb050f04b3
Velk <samuelarsac@hotmail.com>
2022-06-29 12:03:41 +0200

    trigger end

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -6,6 +6,24 @@ using UnityEngine.EventSystems;
 
 public class End : MonoBehaviour
 {
+    [SerializeField] private GameObject transitionSection;
+    [SerializeField] private GameObject fade;
+    [SerializeField] private GameObject block;
+    [SerializeField] private GameObject watches;
+
+    public void DisplayTransition()
+    {
+        transitionSection.SetActive(true);
+        watches.SetActive(false);
+    }
+
+    public void HideTransition()
+    {
+        transitionSection.SetActive(false);
+        fade.SetActive(true);
+        block.SetActive(false);
+    }
+
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
